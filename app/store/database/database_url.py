a20bc357068c9a62b36f48c146bacd6818721af7
waterstark <waterstark97@yandex.ru>
2023-02-28 17:32:21 +0400

    Done v3

--- app/store/database/database_url.py
+++ app/store/database/database_url.py
@@ -1,3 +1,4 @@
 from app.web.config import DatabaseConfig
 
-DATABASE_URL = f"postgresql+asyncpg://{DatabaseConfig.user}:{DatabaseConfig.password}@{DatabaseConfig.host}:{DatabaseConfig.port}/{DatabaseConfig.database}"
+# DATABASE_URL = f"postgresql+asyncpg://{DatabaseConfig.user}:{DatabaseConfig.password}@{DatabaseConfig.host}:{DatabaseConfig.port}/{DatabaseConfig.database}"
+DATABASE_URL = "postgresql+asyncpg://kts_user:kts_pass@0.0.0.0:5432/kts"
